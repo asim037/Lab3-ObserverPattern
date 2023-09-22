d9de3c70486764247479efe5fa18f0b71f50e24c
Asim Tariq <107315975+asim037@users.noreply.github.com>
2023-09-22 11:41:25 -0700

    Add files via upload

---
+++ decoratorss/Shape.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package decoratorss;
+
+/**
+ *
+ * @author LAPTOP HOUSE
+ */
+public interface Shape {
+    void draw();
+    double getPrice();
+}
